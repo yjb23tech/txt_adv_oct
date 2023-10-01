f2994c089e4a47f0919a6141b6c9e7490362a3f9
yjb23tech <yjb23tech@gmail.com>
2023-10-01 14:12:12 +0100

    Ukraine

---
+++ data_strucs.py
@@ -0,0 +1,1 @@
+arr_player_options = ['N to travel North', 'E to travel East', 'S to travel South', 'W to travel West', 'I to reveal your Inventory', 'Q to Quit the Game']
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from classes import Player
 from functions import str_player_name, int_player_age, str_player_location
+from data_strucs import arr_player_options 
 
 test_player = Player("Joshua", 29, "LunDun")
 print(" ")
@@ -10,7 +11,13 @@ test_player_2 = Player(str_player_name(), int_player_age(), str_player_location(
 
 print(" ")
 print(test_player_2)
+print("\nYou have the following options available to you Sailor:\n")
+
+for x, option in enumerate(arr_player_options, 1):
+    print(f"{x}. Press {option}")
 print(" ")
+
+
 
 
 
