f25c93c270fa9ff65bf172d010d56889233dd708
yjb23tech <yjb23tech@gmail.com>
2023-10-01 15:46:46 +0100

    Rock n Roll

--- scrap_book.py
+++ scrap_book.py
@@ -2,17 +2,24 @@ arr_game_grid = [[0, 1, 2], [0, 1, 2], [0, 1, 2]]
 
 arr_game_grid_3_by_3 = [["South West", "West", "North West"], ["South", "Centre", "North"], ["South East", "East", "North East"]]
 
+print(" ")
+
 print(arr_game_grid_3_by_3[0][0])
 print(arr_game_grid_3_by_3[0][1])
 print(arr_game_grid_3_by_3[0][2])
+
+print(" ")
 
 print(arr_game_grid_3_by_3[1][0])
 print(arr_game_grid_3_by_3[1][1])
 print(arr_game_grid_3_by_3[1][2])
 
+print(" ")
+
 print(arr_game_grid_3_by_3[2][0])
 print(arr_game_grid_3_by_3[2][1])
 print(arr_game_grid_3_by_3[2][2])
 
+print(" ")
 
 
