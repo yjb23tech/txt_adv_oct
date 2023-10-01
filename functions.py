7657bf7c3738cf0e597585c27b50597dbadcc579
yjb23tech <yjb23tech@gmail.com>
2023-10-01 14:05:21 +0100

    Rebranding

---
+++ functions.py
@@ -0,0 +1,12 @@
+def str_player_name():
+    player_name = input("What is your name Sailor?\n")
+    return player_name
+
+def int_player_age():
+    player_age = int(input("How old are you Sailor?\n"))
+    return player_age 
+
+def str_player_location():
+    player_location = input("And where are you from Sailor?\n")
+    return player_location
+
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from classes import Player
+from functions import str_player_name, int_player_age, str_player_location
 
 test_player = Player("Joshua", 29, "LunDun")
+print(" ")
 print(test_player)
+print(" ")
+
+test_player_2 = Player(str_player_name(), int_player_age(), str_player_location())
+
+print(" ")
+print(test_player_2)
+print(" ")
+
+
+
 
