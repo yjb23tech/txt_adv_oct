a2674e4e70c7476671f62cbb70d537effbb55a28
yjb23tech <yjb23tech@gmail.com>
2023-10-01 13:57:04 +0100

    One Piece

---
+++ classes.py
@@ -0,0 +1,10 @@
+class Player:
+
+    def __init__(self, str_name, int_age, str_location):
+        self.str_name = str_name
+        self.int_age = int_age
+        self.str_location = str_location
+    
+    def __str__(self):
+        return (f"Welcome {self.str_name} from {self.str_location}! Who knew a {self.int_age} year old sapling throw in his lot to become Pirate King XD")
+
---
+++ main.py
@@ -0,0 +1,5 @@
+from classes import Player
+
+test_player = Player("Joshua", 29, "LunDun")
+print(test_player)
+
